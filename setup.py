eb3dc55ad9e60712358b91a04163307b8c832219
Allan Swanepoel <allanice001@gmail.com>
2019-02-21 20:19:40 +0200

    Extending setup,py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+import os
 from setuptools import setup, find_packages
+
+
+def read(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        return f.read()
 
 setup(
     name='django-zuul',
-    version='1.0.0',
+    version='1.0.1',
     keywords = ('django', 'apigateway', 'dataproxy'),
     description = 'apigateway powered by django',
     install_requires=['django==2.1.7',
@@ -10,5 +16,7 @@ setup(
                       'djangorestframework',
                       'psycopg2-binary'],
     packages=find_packages(exclude=['tests']),
-    include_package_data=True
+    include_package_data=True,
+    long_description=read('README.md'),
+    url='https://github.com/infrascloudy/django-zuul'
 )
